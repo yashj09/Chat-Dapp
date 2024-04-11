28f589b441c9f4e6a7d82ccd5008b93b8546827a
yashj09 <yashj8858@gmail.com>
2024-04-11 23:55:49 +0530

    app done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { PushAPI, CONSTANTS } from "@pushprotocol/restapi";
 // import { ethers } from "ethers";
-import { ChatUIProvider, ChatView } from "@pushprotocol/uiweb";
+import { ChatUIProvider, ChatView, darkChatTheme } from "@pushprotocol/uiweb";
+import { useState } from "react";
 // const signer = ethers.Wallet.createRandom();
 
 // const userAlice = await PushAPI.initialize(signer, {
@@ -10,23 +11,40 @@ import { ChatUIProvider, ChatView } from "@pushprotocol/uiweb";
 
 
 const App = () => {
+  const [receiver, setReceiver] = useState("");
   return (
-    <div className=" mx-96 my-16">
-      <h2>
-        Live chat with pushai.eth, connect your wallet and chat to get sassy
-        response from PushAI.eth
-      </h2>
-      <div style={{ height: "75vh", margin: "20px auto" }}>
-        <ChatUIProvider>
+    <div className="mx-96 my-4 flex justify-center flex-col">
+      <div className="flex justify-center font-semibold text-2xl">
+        <h2>
+          One on one private messaging
+        </h2>
+      </div>
+      <div className=" mt-2 ">
+        <div className="flex justify-center font-semibold text-xl">
+          <h1>Receiver's Address</h1>
+        </div>
+        <div className="flex justify-center ">
+          <input
+            value={receiver}
+            onChange={(e) => setReceiver(e.target.value)}
+            className="border w-[60%] rounded-xl shadow-xl p-4 m-4 mt-1"
+            placeholder="enter receiver's address"
+          />
+        </div>
+      </div>
+      <div className="h-[75vh] m-8 mt-0" >
+        <ChatUIProvider theme={darkChatTheme}>
           <ChatView
-            chatId="0x99A08ac6254dcf7ccc37CeC662aeba8eFA666666"
+            chatId={receiver}
             limit={10}
             isConnected={true}
 
           />
         </ChatUIProvider>
+
       </div>
     </div>
+
   )
 }
 
